a2520edcafd613b019589543ab94f613c80f1106
mstampelou <mstampelou@hotmail.com>
2026-08-29 14:55:06 +0300

    refactor: move @EnableJpaAuditing to dedicated config class

--- src/main/java/gr/aueb/cf/helpdesk/HelpdeskApplication.java
+++ src/main/java/gr/aueb/cf/helpdesk/HelpdeskApplication.java
@@ -2,10 +2,8 @@ package gr.aueb.cf.helpdesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class HelpdeskApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
